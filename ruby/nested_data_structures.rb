b8f08f22a34d8e82b1f32b325e9d062d357f2ffb
Minkee Sung <minkeesung@gmail.com>
2017-02-26 04:33:14 -0500

    made changes to nested data structures

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,7 @@
+highway = {car_1: ['bob', 'steve', 'samantha'], car_2: ['jackie', 'bob', 'corinn'], car_3: ['minkee', 'lora', 'susan']}
+
+p highway[:car_3][2]
+
+p highway[:car_2][-1]
+
+p highway[:car_1][1..-1]
